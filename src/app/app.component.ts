8b1ab8c55ebe509c1f8bc2ca8cb7feae1221b30d
Vector254 <ochrist7@gmail.com>
2020-08-22 17:11:46 +0300

    create form to add quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes-app';
+title = 'The Quotes App';
+quote;
+Author;
+formdata;
+
+ngOnInit() {
+this.formdata = new FormGroup({
+Name: new FormControl(),
+quote: new FormControl(),
+Author: new FormControl()
+});
 }
+onClickSubmit(data) {this.quote = data.quote; this.Author = data.Author;}
+
+}
+
