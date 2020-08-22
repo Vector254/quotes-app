ae47d7f621f59eefb3e1a1b8823a08f0fb698851
Vector254 <ochrist7@gmail.com>
2020-08-22 18:49:36 +0300

    create form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
 selector: 'app-root',
 templateUrl: './app.component.html',
@@ -7,18 +7,5 @@ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 title = 'The Quotes App';
-quote;
-Author;
-formdata;
-
-ngOnInit() {
-this.formdata = new FormGroup({
-Name: new FormControl(),
-quote: new FormControl(),
-Author: new FormControl()
-});
-}
-onClickSubmit(data) {this.quote = data.quote; this.Author = data.Author;}
-
 }
 
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+quote;
+Author;
+formdata;
+
+ngOnInit() {
+this.formdata = new FormGroup({
+Name: new FormControl(),
+quote: new FormControl(),
+Author: new FormControl()
+});
+}
+onClickSubmit(data) {this.quote = data.quote; this.Author = data.Author;}
+
+
+}
