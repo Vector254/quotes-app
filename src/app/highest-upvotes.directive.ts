922e1b4d421a868e1ae386a34e6291bdd54779ff
Vector254 <ochrist7@gmail.com>
2020-08-24 15:07:54 +0300

    add date pipe

--- src/app/highest-upvotes.directive.ts
+++ src/app/highest-upvotes.directive.ts
@@ -1,12 +1,13 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighestUpvotes]'
 })
 export class HighestUpvotesDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.backgroundColor ='yellow';
+  constructor(private elem:ElementRef) {}
+    private textDeco(action:string){
+    this.elem.nativeElement.style.backgroundColor ='green';
    }
 
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,9 @@ import {Quotes} from '../quotes';
 export class QuotesComponent implements OnInit {
   
   sayings:Quotes[] = [
-    new Quotes('The greater the suffering the greater the peace','anonymous','vector'),
-    new Quotes('No matter how much weed satan smokes he can never','turker chris','victor'),
-    new Quotes('We came, we saw, we conquered!','leon james','nduati')
+    new Quotes('The greater the suffering the greater the peace','anonymous','vector',new Date(2020,7,24)),
+    new Quotes('No matter how much weed satan smokes he can never','turker chris','victor',new Date(2020,7,23)),
+    new Quotes('We came, we saw, we conquered!','leon james','nduati',new Date(2020,7,22))
   ];
   toggleDetails(index){
     this.sayings[index].showDescription = !this.sayings[index].showDescription;
